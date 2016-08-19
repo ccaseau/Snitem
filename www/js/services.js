d5bd96d243f95e568b5201fbae632cad06bef204
ccaseau <charlotte.caseau@gmail.com>
2016-08-19 14:41:04 +0200

    Mise en place de l'injection des données
    injection des données + mise en place de la bdd locale sqlite

--- www/js/services.js
+++ www/js/services.js
@@ -85,3 +85,89 @@ app.factory('ManageCustomisation', function(){
 
   }
 })
+
+//Table Questions
+app.factory('QuestionsDataService', function ($cordovaSQLite, $ionicPlatform) {
+
+  return {
+    //Retourne un certain nombre (nb_qst) de question de maniere aléatoire + selectionne les reponses associées
+      getRandomQuestion: function(nb_qst,callback){
+           $ionicPlatform.ready(function () {
+             $cordovaSQLite.execute(db, 'SELECT * FROM Questions ORDER BY RANDOM() LIMIT "'+nb_qst+'"').then(function (resultsQ) {
+
+               var dataQuestion = [];
+               var dataReponse = [];
+               for (i = 0, max = resultsQ.rows.length; i < max; i++) {
+                 dataQuestion.push(resultsQ.rows.item(i))
+               }
+                  for (j = 0 ; j<nb_qst ; j++)
+                  {
+                    $cordovaSQLite.execute(db, 'SELECT reponse1,reponse2,reponse3,reponse4 FROM Reponses WHERE id_question = "' + dataQuestion[j].id + '"').then(function (resultsR) {
+                      //WHERE  id_question = "' + dataQuestion[j].id + '"
+                      for (i = 0, max = resultsR.rows.length; i < max; i++) {
+                        dataReponse.push(resultsR.rows.item(i))
+                      }
+                      callback(dataQuestion,dataReponse);
+                    })
+                  }
+             })
+          })
+        }
+      }
+  })
+
+  app.factory('CadeauxDataService', function ($cordovaSQLite, $ionicPlatform) {
+
+    return {
+    //On recupere la dotation dont l'heure est passée avec l'obligatoire le plus haut puis l'heure la plus haute
+    getCadeau: function(date,date2,callback){
+         $ionicPlatform.ready(function () {
+           $cordovaSQLite.execute(db,'SELECT id,CodeCadeau,Texte FROM Cadeaux WHERE Quantite > 0 AND ShowTime <= "'+date+'" ORDER BY Obligatoire DESC, ShowTime DESC, Id DESC').then(function (results) {
+
+             var data = []
+
+             if (results.rows.length > 0)
+             {
+               for (i = 0, max = results.rows.length; i < max; i++) {
+                 data.push(results.rows.item(i))
+               }
+               callback(data)
+             }
+
+            //On recupere la prochaine dotation disponible du jour
+             else
+             {
+              $cordovaSQLite.execute(db,'SELECT id,CodeCadeau,Texte FROM Cadeaux  WHERE Quantite > 0 AND ShowTime >= "'+date+'" AND ShowTime <="'+date2+'" ORDER BY Obligatoire ASC, ShowTime ASC, Id ASC').then(function (results2) {
+                for (i = 0, max = results2.rows.length; i < max; i++) {
+                  data.push(results2.rows.item(i))
+                }
+                callback(data)
+              })
+            }
+        })
+      })
+    },
+
+    //Soustrait de 1 la quantité de la dotation avec l'id égal à Cadeauid
+    SubstrQuantite: function(Cadeauid){
+       return $cordovaSQLite.execute(db, 'UPDATE Cadeaux SET Quantite=(Quantite-1) WHERE Id = ?', [Cadeauid])
+     },
+  }
+  })
+
+  app.factory('ThemesDataService', function ($cordovaSQLite, $ionicPlatform, $http) {
+    return {
+      //Retourner le théme dans la bdd
+      getAll: function(callback){
+           $ionicPlatform.ready(function () {
+             $cordovaSQLite.execute(db, 'SELECT * FROM Themes').then(function (results) {
+               var data = []
+               for (i = 0, max = results.rows.length; i < max; i++) {
+                 data.push(results.rows.item(i))
+               }
+               callback(data)
+             })
+          })
+        }
+      }
+      })
